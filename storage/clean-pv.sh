963fa04daefb7a396a2d9aa1a34bc1ed14f8f18b
AJ Pahl <ajpahl1008@gmail.com>
2023-12-01 10:19:20 -0500

    cleaned up pvc directory. added kube4 to cluster

--- storage/clean-pv.sh
+++ storage/clean-pv.sh
@@ -2,3 +2,4 @@ echo "Cleaning up old persistent volumes"
 kubectl delete -f eck1-pv.yml -n elastic-system
 kubectl delete -f eck2-pv.yml -n elastic-system
 kubectl delete -f eck3-pv.yml -n elastic-system
+kubectl delete -f eck4-pv.yml -n elastic-system
--- storage/restart.sh
+++ storage/restart.sh
@@ -2,7 +2,9 @@ echo "Cleaning up old persistent volumes"
 kubectl delete -f eck1-pv.yml -n elastic-system
 kubectl delete -f eck2-pv.yml -n elastic-system
 kubectl delete -f eck3-pv.yml -n elastic-system
+kubectl delete -f eck4-pv.yml -n elastic-system
 echo "Adding new persistent volumes"
 kubectl apply -f eck1-pv.yml -n elastic-system
 kubectl apply -f eck2-pv.yml -n elastic-system
 kubectl apply -f eck3-pv.yml -n elastic-system
+kubectl apply -f eck4-pv.yml -n elastic-system
